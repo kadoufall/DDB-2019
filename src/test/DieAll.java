4ba3660593e8bacc550f6812bea434263ed04c9c
chaoyanglv <34942365+chaoyanglv@users.noreply.github.com>
2019-08-02 11:48:24 +0800

    Update DieAll.java

--- src/test/DieAll.java
+++ src/test/DieAll.java
@@ -2,9 +2,9 @@ package test;
 
 import transaction.WorkflowController;
 import test.Connector;
+import transaction.exceptions.TransactionAbortedException;
 
-public class DieAll {
-	
+public class DieAll{
 	public static void main(String[] a){
 		Connector.cleanData();
 		Connector.launch("ALL");
@@ -13,26 +13,64 @@ public class DieAll {
 		 try{
 			int xid;
 
-			xid = wc.start();			
-			wc.addFlight(xid, "347", 100, 310);
+			xid = wc.start();	
+			wc.addRooms(xid, "handan", 1000, 999);
+			wc.dieNow("ALL");
+			wc.addCars(xid, "BYD", 300, 55);
+			wc.commit(xid);	
+			/*wc.addFlight(xid, "347", 100, 310);
 			wc.addRooms(xid, "Stanford", 200, 150);	
 			wc.addCars(xid, "SFO", 300, 30);
 			wc.newCustomer(xid, "John");		
-			wc.commit(xid);	
-			wc.dieNow("ALL");
-			/////////// except java.rmi.RemoteException 
-                                                //////////  launch ALL		
+			wc.commit(xid);			
 			
 			xid = wc.start();
-			wc.queryFlight(xid, "347");
-			wc.queryFlightPrice(xid, "347");
-			wc.queryRooms(xid, "Stanford");
-			wc.queryRoomsPrice(xid, "Stanford");
-			wc.queryCars(xid, "SFO");
-			wc.queryCarsPrice(xid, "SFO");
-			wc.queryCustomerBill(xid, "John");
+			wc.addFlight(xid, "347", 100, 620);
+			wc.addRooms(xid, "Stanford", 200, 300);	
+			wc.addCars(xid, "SFO", 300, 60);	
+			wc.dieRMAfterPrepare("RMFlights");
+			wc.commit(xid);
+			////////////////except transaction.exceptions.TransactionAbortedException
+			////////////////launch RMFlights
+			Connector.launch("RMFlights");
+			wc.reconnect();
+
+			xid = wc.start();
+			int r1 = wc.queryFlight(xid, "347");
+			check(100, r1);
+			int r2 = wc.queryFlightPrice(xid, "347");
+			check(310, r2);
+			int r3 = wc.queryRooms(xid, "Stanford");
+			check(200, r3);
+			int r4 = wc.queryRoomsPrice(xid, "Stanford");
+			check(150, r4);
+			int r5 = wc.queryCars(xid, "SFO");
+			check(300, r5);
+			int r6 = wc.queryCarsPrice(xid, "SFO");
+			check(30, r6);
+			int r7 = wc.queryCustomerBill(xid, "John");
+			check(0, r7);
+			*/
+			//wc.commit(xid);
+			System.out.println("Test pass.");	
+			//System.out.println("Test pass:" + DieRMAfterPrepare.class.getName());	
+			
 		 }catch(Exception e){
-			 System.out.println("DieAll exception "+e.getMessage());
-		 }
+		 	if (e.getClass().getName().equals(TransactionAbortedException.class.getName())) {
+    			System.out.println("Test pass!");
+			} else {
+    			System.out.println("Test fail:" + e);
+			}
+			//System.out.println("Test fail:"+e.getMessage());
+			//The transaction 1 aborted:commit prepare
+		}finally {
+            Connector.cleanUpExit();
+        }
 	}
+	private static void check(int expect, int real) {
+        if (expect != real) {
+            System.out.println(expect + " " + real);
+            System.err.println("Test fail");
+        }
+    }
 }
