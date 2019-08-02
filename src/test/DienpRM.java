1afca13b8169ef6bfd99e42c5ff37f0b9d598377
chaoyanglv <34942365+chaoyanglv@users.noreply.github.com>
2019-08-02 11:47:09 +0800

    Update DienpRM.java

--- src/test/DienpRM.java
+++ src/test/DienpRM.java
@@ -2,6 +2,9 @@ package test;
 
 import transaction.WorkflowController;
 import test.Connector;
+import transaction.exceptions.TransactionAbortedException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DienpRM {
 
@@ -14,31 +17,62 @@ public class DienpRM {
             int xid;
 
             xid = wc.start();
-            wc.addFlight(xid, "347", 100, 310);
+            wc.addFlight(xid, "MU5377", 100, 500);
+            wc.addRooms(xid, "shanghai", 300, 350);
+            wc.addCars(xid, "BYD", 100, 30);
+            wc.newCustomer(xid, "CYLV");
+            /*wc.addFlight(xid, "347", 100, 310);
             wc.addRooms(xid, "Stanford", 200, 150);
             wc.addCars(xid, "SFO", 300, 30);
-            wc.newCustomer(xid, "John");
+            wc.newCustomer(xid, "John");*/
             wc.commit(xid);
 
             xid = wc.start();
             //////////wc.reserveItinerary xid "John" (347) "SFO" false false
-            wc.addRooms(xid, "Stanford", 200, 300);
+            List<String> flights = new ArrayList<>();
+            flights.add("347");
+            wc.reserveItinerary(xid, "CYLV", flights, "BYD", false, false);
+            wc.addRooms(xid, "shanghai", 200, 300);
             wc.dieNow("RMCars");
             /////////launch RMCars
+            Connector.launch("RMCars");
             wc.reconnect();
             wc.commit(xid);
 
 
             xid = wc.start();
-            wc.queryFlight(xid, "347");
-            wc.queryFlightPrice(xid, "347");
-            wc.queryRooms(xid, "Stanford");
-            wc.queryRoomsPrice(xid, "Stanford");
-            wc.queryCars(xid, "SFO");
-            wc.queryCarsPrice(xid, "SFO");
-            wc.queryCustomerBill(xid, "John");
+            int r1 = wc.queryFlight(xid, "MU5377");
+            check(100, r1);
+            int r2 = wc.queryFlightPrice(xid, "MU5377");
+            check(310, r2);
+            int r3 = wc.queryRooms(xid, "shanghai");
+            check(500, r3);
+            int r4 = wc.queryRoomsPrice(xid, "shanghai");
+            check(300, r4);
+            int r5 = wc.queryCars(xid, "BYD");
+            check(100, r5);
+            int r6 = wc.queryCarsPrice(xid, "BYD");
+            check(30, r6);
+            int r7 = wc.queryCustomerBill(xid, "CYLV");
+            check(0, r7);
+            System.out.println("Test pass."); 
         } catch (Exception e) {
-            System.out.println("DienpRM exception " + e.getMessage());
+            //System.out.println("Test fail:" + e.getMessage());
+            if (e.getClass().getName().equals(TransactionAbortedException.class.getName())) {
+                System.out.println("Test pass!");
+            } else {
+                System.out.println("Test fail:" + e);
+            }
+            //System.out.println("Test fail:"+e.getMessage());
+            //The transaction 1 aborted:commit prepare
+        }finally {
+            Connector.cleanUpExit();
+        }
+    }
+    private static void check(int expect, int real) {
+        if (expect != real) {
+            System.out.println(expect + " " + real);
+            System.err.println("Test fail");
         }
     }
 }
