6d9247805a009b5a5ef80ace686207ed76c49b9b
chaoyanglv <34942365+chaoyanglv@users.noreply.github.com>
2019-08-02 10:41:29 +0800

    Update and rename Baddcmt.java to data_addCommit.java

---
+++ src/test/data_addCommit.java
@@ -0,0 +1,27 @@
+package test;
+
+import test.Connector;
+import transaction.WorkflowController;
+
+public class data_addCommit {
+
+    public static void main(String[] a) {
+        Connector.cleanData();
+        Connector.launch("ALL");
+
+        WorkflowController wc = Connector.connectWC();
+        try {
+            int xid = wc.start();
+            wc.addFlight(xid, "MU5377", 100, 500);
+            wc.addRooms(xid, "shanghai", 300, 350);
+            wc.addCars(xid, "BYD", 100, 30);
+            wc.newCustomer(xid, "CYLV");
+            wc.commit(xid);
+            System.out.println("Test pass:" + data_addCommit.class.getName());
+        }catch (Exception e) {
+            System.out.println("Test fail:" + e.getMessage());
+        }finally {
+            Connector.cleanUpExit();
+        }
+    }
+}
