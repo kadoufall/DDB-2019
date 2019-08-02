4cd5bc3d9087dee2f23623c1b85b30e2e1985220
chaoyanglv <34942365+chaoyanglv@users.noreply.github.com>
2019-08-02 11:42:06 +0800

    Update DieWC.java

--- src/test/DieWC.java
+++ src/test/DieWC.java
@@ -2,6 +2,10 @@ package test;
 
 import test.Connector;
 import transaction.WorkflowController;
+import java.util.ArrayList;
+import java.util.List;
+import transaction.exceptions.TransactionAbortedException;
+
 
 public class DieWC {
 
@@ -14,30 +18,55 @@ public class DieWC {
             int xid;
 
             xid = wc.start();
-            wc.addFlight(xid, "347", 100, 310);
-            wc.addRooms(xid, "Stanford", 200, 150);
-            wc.addCars(xid, "SFO", 300, 30);
-            wc.newCustomer(xid, "John");
+            wc.addFlight(xid, "MU5377", 100, 500);
+            wc.addRooms(xid, "shanghai", 300, 350);
+            wc.addCars(xid, "BYD", 100, 30);
+            wc.newCustomer(xid, "CYLV");
             wc.commit(xid);
 
             xid = wc.start();
             //////////wc.reserveItinerary xid "John" (347) "SFO" true true
+            List<String> flights = new ArrayList<>();
+            flights.add("MU5377");
+            wc.reserveItinerary(xid, "CYLV", flights, "BYD", true, true);
             wc.dieNow("WC");
             /////////except java.rmi.RemoteException
             /////////launch WC
+            Connector.launch("WC");
             wc.commit(xid);
 
 
             xid = wc.start();
-            wc.queryFlight(xid, "347");
-            wc.queryFlightPrice(xid, "347");
-            wc.queryRooms(xid, "Stanford");
-            wc.queryRoomsPrice(xid, "Stanford");
-            wc.queryCars(xid, "SFO");
-            wc.queryCarsPrice(xid, "SFO");
-            wc.queryCustomerBill(xid, "John");
+            int r1 = wc.queryFlight(xid, "347");
+            check(99, r1);
+            int r2 = wc.queryFlightPrice(xid, "347");
+            check(310, r2);
+            int r3 = wc.queryRooms(xid, "Stanford");
+            check(299, r3);
+            int r4 = wc.queryRoomsPrice(xid, "Stanford");
+            check(350, r4);
+            int r5 = wc.queryCars(xid, "SFO");
+            check(99, r5);
+            int r6 = wc.queryCarsPrice(xid, "SFO");
+            check(30, r6);
+            int r7 = wc.queryCustomerBill(xid, "John");
+            check(880, r7);
+            System.out.println("Test pass."); 
         } catch (Exception e) {
-            System.out.println("DieWC exception " + e.getMessage());
+            if (e.getClass().getName().equals(TransactionAbortedException.class.getName())) {
+                System.out.println("Test pass!");
+            } else {
+                System.out.println("Test fail:" + e);
+            }
+            //System.out.println("Test fail:" + e.getMessage());
+        }finally {
+            Connector.cleanUpExit();
+        }
+    }
+    private static void check(int expect, int real) {
+        if (expect != real) {
+            System.out.println(expect + " " + real);
+            System.err.println("Test fail");
         }
     }
 }
