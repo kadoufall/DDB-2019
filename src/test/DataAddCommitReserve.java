1d6c193531cfe4dc8af5a65c8bdd9cd6b3b94c86
chaoyanglv <34942365+chaoyanglv@users.noreply.github.com>
2019-08-02 12:30:58 +0800

    Update and rename Baddcmtrsv.java to DataAddCommitReserve.java

---
+++ src/test/DataAddCommitReserve.java
@@ -0,0 +1,37 @@
+package test;
+
+import test.Connector;
+import transaction.WorkflowController;
+
+public class DataAddCommitReserve{
+
+    public static void main(String[] a) {
+        Connector.cleanData();
+        Connector.launch("ALL");
+
+        WorkflowController wc = Connector.connectWC();
+        try {
+            int xid = wc.start();
+            /*wc.addFlight(xid, "347", 100, 310);
+            wc.addRooms(xid, "Stanford", 200, 150);
+            wc.addCars(xid, "SFO", 300, 30);
+            wc.newCustomer(xid, "John");*/
+            wc.addFlight(xid, "MU5377", 100, 500);
+            wc.addRooms(xid, "shanghai", 300, 350);
+            wc.addCars(xid, "BYD", 100, 30);
+            wc.newCustomer(xid, "CYLV");
+            wc.commit(xid);
+
+            xid = wc.start();
+            wc.reserveFlight(xid, "CYLV", "MU5377");
+            wc.reserveRoom(xid, "CYLV", "shanghai");
+            wc.reserveCar(xid, "CYLV", "BYD");
+            wc.commit(xid);
+            System.out.println("Test pass.");
+        } catch (Exception e) {
+            System.out.println("Test fail:" + e.getMessage());
+        }finally {
+            Connector.cleanUpExit();
+        }
+    }
+}
