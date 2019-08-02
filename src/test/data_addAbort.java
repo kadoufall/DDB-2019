b3ab93ffc0ecd182bd0f15b4b4c1624fefb86cf4
chaoyanglv <34942365+chaoyanglv@users.noreply.github.com>
2019-08-02 10:32:59 +0800

    Update and rename Baddabt.java to data_addAbort.java

--- src/test/data_addAbort.java
+++ src/test/data_addAbort.java
@@ -1,7 +1,9 @@
+package test;
+
 import test.Connector;
 import transaction.WorkflowController;
 
-public class Baddabt {
+public class data_addabort {
 
     public static void main(String[] a) {
         Connector.cleanData();
@@ -10,13 +12,14 @@ public class Baddabt {
         WorkflowController wc = Connector.connectWC();
         try {
             int xid = wc.start();
-            wc.addFlight(xid, "347", 100, 310);
-            wc.addRooms(xid, "Stanford", 200, 150);
-            wc.addCars(xid, "SFO", 300, 30);
-            wc.newCustomer(xid, "John");
+            wc.addFlight(xid, "MU5377", 100, 500);
+            wc.addRooms(xid, "shanghai", 300, 350);
+            wc.addCars(xid, "BYD", 100, 30);
+            wc.newCustomer(xid, "CYLV");
             wc.abort(xid);
+            System.out.println("Test pass.");
         } catch (Exception e) {
-            System.out.println("insert data exception " + e.getMessage());
+            System.out.println("Test fail:" + e.getMessage());
         } finally {
             Connector.cleanUpExit();
         }
