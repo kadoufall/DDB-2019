96aa7c44128c13e3d48a62e2e38ee6af71370aef
chaoyanglv <34942365+chaoyanglv@users.noreply.github.com>
2019-08-03 15:33:56 +0800

    Update DieRMAfterPrepare.java

--- src/test/DieRMAfterPrepare.java
+++ src/test/DieRMAfterPrepare.java
@@ -1,6 +1,7 @@
 package test;
 
 import transaction.WorkflowController;
+import transaction.exceptions.TransactionAbortedException;
 
 public class DieRMAfterPrepare {
 	public static void main(String[] a){
@@ -23,24 +24,51 @@ public class DieRMAfterPrepare {
 			wc.addRooms(xid, "Stanford", 200, 300);	
 			wc.addCars(xid, "SFO", 300, 60);	
 			wc.dieRMAfterPrepare("RMFlights");
-			wc.commit(xid);
+			//wc.commit(xid);
+			try {
+                wc.commit(xid);
+            } catch (Exception e) {
+                // e.printStackTrace();
+            }
 			////////////////except transaction.exceptions.TransactionAbortedException
 			////////////////launch RMFlights
+			Connector.launch("RMFlights");
 			wc.reconnect();
 
 			xid = wc.start();
-			wc.queryFlight(xid, "347");
-			wc.queryFlightPrice(xid, "347");
-			wc.queryRooms(xid, "Stanford");
-			wc.queryRoomsPrice(xid, "Stanford");
-			wc.queryCars(xid, "SFO");
-			wc.queryCarsPrice(xid, "SFO");
-			wc.queryCustomerBill(xid, "John");
+			int r1 = wc.queryFlight(xid, "347");
+            check(100, r1, "queryFlight");
 
-			 Connector.cleanUpExit(0);
+            int r2 = wc.queryFlightPrice(xid, "347");
+            check(310, r2, "queryFlightPrice");
+
+            int r3 = wc.queryRooms(xid, "Stanford");
+            check(200, r3, "queryRooms");
+
+            int r4 = wc.queryRoomsPrice(xid, "Stanford");
+            check(150, r4, "queryRoomsPrice");
+
+            int r5 = wc.queryCars(xid, "SFO");
+            check(300, r5, "queryCars");
+
+            int r6 = wc.queryCarsPrice(xid, "SFO");
+            check(30, r6, "queryCarsPrice");
+
+            int r7 = wc.queryCustomerBill(xid, "John");
+            check(0, r7, "queryCustomerBill");
+            wc.commit(xid);
+
+			Connector.cleanUpExit(0);
 		 }catch(Exception e){
 			 System.out.println("DieRMAfterPrepare exception "+e.getMessage());
 			 Connector.cleanUpExit(1);
 		 }
 	}
+	private static void check(int expect, int real, String method) {
+        if (expect != real) {
+            System.out.println(expect + " " + real);
+            System.err.println("Test fail: " + method);
+            Connector.cleanUpExit(1);
+        }
+    }
 }
