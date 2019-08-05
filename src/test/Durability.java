c1fd1def42a48d25b6c6edb5e18215b96319a58f
chaoyanglv <34942365+chaoyanglv@users.noreply.github.com>
2019-08-05 15:40:33 +0800

    Create Durability.java

---
+++ src/test/Durability.java
@@ -0,0 +1,50 @@
+package test;
+
+import test.Connector;
+import transaction.WorkflowController;
+import transaction.exceptions.TransactionAbortedException;
+import java.rmi.RemoteException;
+
+public class Durability{
+
+    public static void main(String[] a) {
+        Connector.cleanData();
+        Connector.launch("ALL");
+
+        WorkflowController wc = Connector.connectWC();
+        try {
+            int xid = wc.start();
+            wc.addFlight(xid, "MU5377", 100, 500);
+            wc.addRooms(xid, "HANDAN", 300, 350);
+            wc.dieTMAfterCommit();
+            //wc.commit(xid);
+            try {
+                wc.commit(xid);
+            } catch (RemoteException e) {
+                // e.printStackTrace();
+            }
+
+            Connector.launch("TM");
+            //wc.reconnect();
+            int r1 = wc.queryFlight(xid, "MU5377");
+            check(100, r1);
+            int r2 = wc.queryRooms(xid, "HANDAN");
+            check(300, r2);
+            wc.commit(xid);
+            
+            System.out.println("Test pass.");
+            Connector.cleanUpExit(0);
+        } catch (Exception e) {
+            System.out.println("Test fail:" + e.getMessage());
+            Connector.cleanUpExit(1);
+        }
+    }
+
+    private static void check(int expect, int real) {
+        if (expect != real) {
+            System.out.println(expect + " " + real);
+            System.err.println("Test fail");
+            Connector.cleanUpExit(1);
+        }
+    }
+}
