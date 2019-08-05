6d8c016f691fce234882fe04f40a432250c19d40
chaoyanglv <34942365+chaoyanglv@users.noreply.github.com>
2019-08-05 18:12:45 +0800

    Update Isolation.java

--- src/test/Isolation.java
+++ src/test/Isolation.java
@@ -14,27 +14,22 @@ public class Isolation{
             int xid1 = wc.start();
             wc.addFlight(xid1, "MU5377", 100, 500);
             wc.commit(xid1);
-
             xid1 = wc.start();
             wc.addFlight(xid1, "MU5377", 100, 450);
-
             int xid2 = wc.start();
             try {
                 wc.addFlight(xid2, "MU5377", 200, 300);
             } catch (TransactionAbortedException e) {
                 // e.printStackTrace();
             }
-           
             int r1 = wc.queryFlight(xid1, "MU5377");
             check(200, r1);
             wc.commit(xid1);
-
             xid2 = wc.start();
             wc.addFlight(xid2, "MU5377", 400, 3000);
             int r2 = wc.queryFlight(xid2, "MU5377");
             check(600, r2);
             wc.commit(xid2);
-            
             System.out.println("Test pass.");
             Connector.cleanUpExit(0);
         } catch (Exception e) {
