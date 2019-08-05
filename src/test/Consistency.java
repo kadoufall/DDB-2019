f66334a2be0892f5c9601cc01eeac45e6035c470
chaoyanglv <34942365+chaoyanglv@users.noreply.github.com>
2019-08-05 11:55:37 +0800

    Create Consistency.java

---
+++ src/test/Consistency.java
@@ -0,0 +1,53 @@
+package test;
+
+import test.Connector;
+import transaction.WorkflowController;
+import transaction.exceptions.TransactionAbortedException;
+import java.rmi.RemoteException;
+
+public class Consistency{
+
+    public static void main(String[] a) {
+        Connector.cleanData();
+        Connector.launch("ALL");
+
+        WorkflowController wc = Connector.connectWC();
+        try {
+            int xid = wc.start();
+            wc.newCustomer(xid, "John");
+            wc.newCustomer(xid, "Danny");
+            wc.newCustomer(xid, "Jenny");
+
+            wc.addRooms(xid, "SHANGHAI", 200, 350);
+            int r1 = wc.queryRooms(xid, "SHANGHAI");
+            check(200, r1);
+            wc.commit(xid);
+
+            xid = wc.start();
+            wc.reserveRoom(xid, "John", "SHANGHAI");
+            wc.reserveRoom(xid, "Danny", "SHANGHAI");
+            wc.reserveRoom(xid, "Jenny", "SHANGHAI");
+            wc.commit(xid);
+
+            xid = wc.start();
+            int r2 = wc.queryRooms(xid, "SHANGHAI");
+            check(197, r2);
+            
+            
+            
+            System.out.println("Test pass.");
+            Connector.cleanUpExit(0);
+        } catch (Exception e) {
+            System.out.println("Test fail:" + e.getMessage());
+            Connector.cleanUpExit(1);
+        }
+    }
+
+    private static void check(int expect, int real) {
+        if (expect != real) {
+            System.out.println(expect + " " + real);
+            System.err.println("Test fail");
+            Connector.cleanUpExit(1);
+        }
+    }
+}
