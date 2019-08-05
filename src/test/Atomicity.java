f426d857e56ba208b2da7ffebdab36c5ecbef3c1
chaoyanglv <34942365+chaoyanglv@users.noreply.github.com>
2019-08-05 18:07:05 +0800

    Update Atomicity.java

--- src/test/Atomicity.java
+++ src/test/Atomicity.java
@@ -10,34 +10,25 @@ public class Atomicity {
     public static void main(String[] a) {
         Connector.cleanData();
         Connector.launch("ALL");
-
         WorkflowController wc = Connector.connectWC();
         try {
-
             int xid = wc.start();
             wc.addFlight(xid, "MU5377", 100, 500);
             wc.dieTMBeforeCommit();
-
             try {
                 wc.commit(xid);
             } catch (RemoteException e) {
                 // e.printStackTrace();
             }
-
             Connector.launch("TM");
-
             wc.reconnect();
-
-            //xid = wc.start();
             int r1 = wc.queryFlight(xid, "MU5377");
             check(-1, r1);
-
             try {
                 wc.commit(xid);
             } catch (TransactionAbortedException e) {
                 // e.printStackTrace();
             }
-
             System.out.println("Test pass.");
             Connector.cleanUpExit(0);
         } catch (Exception e) {
@@ -45,7 +36,6 @@ public class Atomicity {
             Connector.cleanUpExit(1);
         }
     }
-
     private static void check(int expect, int real) {
         if (expect != real) {
             System.out.println(expect + " " + real);
