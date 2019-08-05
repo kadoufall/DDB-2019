d93bfd840309f6d3231722c66a94b9b3431e2ab8
chaoyanglv <34942365+chaoyanglv@users.noreply.github.com>
2019-08-05 15:41:16 +0800

    Create Isolation.java

---
+++ src/test/Isolation.java
@@ -0,0 +1,55 @@
+package test;
+
+import test.Connector;
+import transaction.WorkflowController;
+import transaction.exceptions.TransactionAbortedException;
+import java.rmi.RemoteException;
+
+public class Isolation{
+
+    public static void main(String[] a) {
+        Connector.cleanData();
+        Connector.launch("ALL");
+
+        WorkflowController wc = Connector.connectWC();
+        try {
+            int xid1 = wc.start();
+            wc.addFlight(xid1, "MU5377", 100, 500);
+            wc.commit(xid1);
+
+            xid1 = wc.start();
+            wc.addFlight(xid1, "MU5377", 100, 450);
+
+            int xid2 = wc.start();
+            try {
+                wc.addFlight(xid2, "MU5377", 200, 300);
+            } catch (TransactionAbortedException e) {
+                // e.printStackTrace();
+            }
+           
+            int r1 = wc.queryFlight(xid1, "MU5377");
+            check(200, r1);
+            wc.commit(xid1);
+
+            xid2 = wc.start();
+            wc.addFlight(xid2, "MU5377", 400, 3000);
+            int r2 = wc.queryRooms(xid2, "MU5377");
+            check(600, r2);
+            wc.commit(xid2);
+            
+            System.out.println("Test pass.");
+            Connector.cleanUpExit(0);
+        } catch (Exception e) {
+            System.out.println("Test fail:" + e.getMessage());
+            Connector.cleanUpExit(1);
+        }
+    }
+
+    private static void check(int expect, int real) {
+        if (expect != real) {
+            System.out.println(expect + " " + real);
+            System.err.println("Test fail");
+            Connector.cleanUpExit(1);
+        }
+    }
+}
